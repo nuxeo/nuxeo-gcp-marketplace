0f0dcdbaba9d605b085978eaa61caaeff955411c
Antoine Taillefer <ataillefer@nuxeo.com>
2024-11-28 23:55:05 +0100

    NXP-32953: Create K8s app package

---
+++ tester/scripts/running-status.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+curlCommand="curl $1/runningstatus -v --retry $2 --retry-connrefused"
+echo "Ruuning command: $curlCommand"
+runningStatus=$($curlCommand)
+echo Running status: $runningStatus
+
+runtimeStatus=$(echo $runningStatus | jq -r '.runtimeStatus')
+repositoryStatus=$(echo $runningStatus | jq -r '.repositoryStatus')
+streamStatus=$(echo $runningStatus | jq -r '.streamStatus')
+
+if [ "$runtimeStatus" = 'ok' -a "$repositoryStatus" = 'ok' -a "$streamStatus" = 'ok' ]; then
+  exit 0
+else
+  exit 1
+fi
